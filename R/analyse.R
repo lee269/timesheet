8bc220adab4fc76e612393c41a965478b51607f1
David Lee <david@lee269.co.uk>
2025-05-04 16:25:37 +0100

    read function

---
+++ R/analyse.R
@@ -0,0 +1,16 @@
+library(here)
+library(unpivotr)
+library(tidyxl)
+library(dplyr)
+
+source(here("R", "read.R"))
+
+ts <- here("data", "2024-01-hours.xlsx")
+sheets <- tidyxl::xlsx_sheet_names(ts)
+sheets <- sheets[1:length(sheets)-1]
+
+data <- sheets |> 
+  purrr::map(\(x) read_timesheet(xlsx = ts, sheet = x)) |> 
+  bind_rows()
+
+data |> count(note)
--- R/read.R
+++ R/read.R
@@ -1,22 +1,19 @@
-library(here)
-library(unpivotr)
-library(tidyxl)
-library(dplyr)
 
-ts <- here("data", "2024-01-hours.xlsx")
+read_timesheet <- function(xlsx, sheet) {
+  
+  cells <- tidyxl::xlsx_cells(path = xlsx, sheets = sheet)
+  
+  data <- cells |> 
+    dplyr::filter(row >=11, col %in% c(2,3,17, 19,20,24)) |> 
+    unpivotr::behead("left", name = "weekday") |> 
+    unpivotr::behead("left", name = "date") |> 
+    unpivotr::behead("left", name = "credit") |> 
+    unpivotr::behead("left", name = "hr") |> 
+    unpivotr::behead("left", name = "min") |> 
+    dplyr::select(sheet, weekday, date = date.header, credit, hr, min, note = character) |> 
+    dplyr::filter(!is.na(date))
+  
+  return(data)
 
-sheets <- tidyxl::xlsx_sheet_names(ts)
+}
 
-cells <- xlsx_cells(path = ts, sheets = "02-Dec-24")
-
-data <- cells |> 
-  filter(row >=11, col %in% c(2,3,17, 19,20,24)) |> 
-  behead("left", name = "weekday") |> 
-  behead("left", name = "date") |> 
-  behead("left", name = "credit") |> 
-  behead("left", name = "hr") |> 
-  behead("left", name = "min") |> 
-  select(sheet, weekday, date = date.header, credit, hr, min, note = character) |> 
-  filter(!is.na(date))
-
-data |> filter(is.na(credit)) |> count(note)
